9686db21927c81576708a38be86e010cb29779e1
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-10 19:59:53 -0400

    adding docstrings

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
+'''This script starts a Flask web application'''
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+'''Creates a Flask application instance'''
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    '''Will display the following string when accessed'''
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    '''Will display the following string when accessed'''
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    '''Will display the following string when accessed. Receives a value in
+    the text variable and replaces '_' with spaces'''
     return 'C ' + str(text.replace('_', ' '))
 
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
 def python_text(text='is cool'):
+    '''Will display the following string when accessed. Receives a value in
+    the text variable and replaces '_' with spaces. If no value is written,
+    it will use the default'''
     return 'Python ' + str(text.replace('_', ' '))
 
 @app.route('/number/<n>', strict_slashes=False)
 def number(n):
+    '''Will display the following string when accessed only if the value
+    received is an integer'''
     if n.isdigit():
         return n + ' is a number'
     else:
@@ -29,6 +41,8 @@ def number(n):
     
 @app.route('/number_template/<n>', strict_slashes=False)
 def number_template(n):
+    '''Will display an HTML page when accessed only if the value received is
+    an integer'''
     if n.isdigit():
         return render_template('5-number.html', value=int(n))
     else:
