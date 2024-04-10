c8e04503aae254189cbce2e970ecca1368990f10
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-10 19:17:34 -0400

    adding docstrings

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+'''This script starts a Flask web application'''
 from flask import Flask
 
+
 app = Flask(__name__)
+'''Creates a Flask application instance'''
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    '''Will display the following string when accessed'''
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    '''Will display the following string when accessed'''
     return 'HBNB'
 
 if __name__ == '__main__':
