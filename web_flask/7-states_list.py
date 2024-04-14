2eaa15e7b03befe1148a104ad3f2be97c38c0203
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-14 12:46:56 -0400

    trying to fix tasks

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,7 @@ def states():
     sorted by name'''
     states = storage.all(State)
     sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template('7-states_list.html', s_states=sorted_states)
 
 
 @app.teardown_appcontext
